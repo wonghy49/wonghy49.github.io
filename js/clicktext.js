f810d9033430bde805802c384c46d5d6387a7744
Wonghhy449 <872946951@qq.com>
2019-04-09 17:53:34 +0800

    Site updated: 2019-04-09 17:53:31

--- js/clicktext.js
+++ js/clicktext.js
@@ -1,3 +1,3 @@
-// build time:Tue Apr 09 2019 16:26:25 GMT+0800 (GMT+08:00)
+// build time:Tue Apr 09 2019 17:51:12 GMT+0800 (GMT+08:00)
 var a_idx=0;$(document).ready(function(){$("body").click(function(e){var t=new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");var o=$("<span/>").text(t[a_idx]);a_idx=(a_idx+1)%t.length;var a=e.pageX,i=e.pageY;o.css({"z-index":9999999,top:i-20,left:a,position:"absolute","font-weight":"bold",color:"#ff6651"});$("body").append(o);o.animate({top:i-180,opacity:0},3e3,function(){o.remove()})});setTimeout("delay()",2e3)});function delay(){$(".buryit").removeAttr("onclick")}
 //rebuild by neat 
