2413222839b46a9323090504cdb23c6606c947a2
Wonghy449 <872946951@qq.com>
2019-06-18 11:29:30 +0800

    Site updated: 2019-06-18 11:29:26

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 18 2019 10:52:54 GMT+0800 (GMT+08:00)
+// build time:Tue Jun 18 2019 11:28:46 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/trhx2.png");document.title="ヽ(●-`Д´-)ノ你丑你就走！";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/img/trhx2.png");document.title="ヾ(Ő∀Ő3)ノ你帅就回来！"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
